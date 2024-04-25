d67f2e1c20d45a91703796eff380d89000a318ab
Ramlov <mathias.ramlov10@gmail.com>
2024-04-25 11:54:10 +0200

    Update sensor_manager.cpp

--- P4-Project/src/sensor_manager.cpp
+++ P4-Project/src/sensor_manager.cpp
@@ -1,4 +1,12 @@
 #include "sensor_manager.h"
+/*
+SensorModule sensorModule(A1, 500, 200, 17); // A1: Moisture sensor pin, 500: Dry reading, 200: Wet reading, 17: DS18B20 digital data pin
+
+float temperature = sensorModule.getTemperature();
+int moisturePercentage = sensorModule.getMoisture();
+
+*/
+
 
 SensorModule::SensorModule(int moisturePin, int dry, int wet, int tempPin)
     : moisturePin(moisturePin), dryReading(dry), wetReading(wet), oneWire(tempPin), tempSensor(&oneWire) {
