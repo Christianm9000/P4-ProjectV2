820d68db8b482e03e01010626b0fe3f1a139b872
Ramlov <mathias.ramlov10@gmail.com>
2024-04-30 09:21:21 +0200

    corrected for time in dataanal

--- P4-Project/S3-Data-Analyst/test.py
+++ P4-Project/S3-Data-Analyst/test.py
@@ -1,5 +1,5 @@
 import pymysql, json
-from datetime import datetime
+from datetime import datetime, timedelta
 
 with open('config.json', 'r') as file:
     config = json.load(file)
@@ -14,6 +14,10 @@ data = cursor.fetchall()
 conn.close()
 
 times = [datetime.fromtimestamp(row[0] / 1000).replace(microsecond=0) for row in data if row[0]]
+timestamps = []
+for time, value in zip(times, values):
+    number_of_measurements = len(bin(int(value, 16))[2:]) // 7
+    timestamps.extend([time - timedelta(minutes=30*i) for i in range(number_of_measurements-1, -1, -1)])
 values = [row[1] for row in data if row[1]]
 numbers = [int(value, 16) for value in values]
 bits = [bin(number)[2:] for number in numbers]
@@ -24,6 +28,4 @@ temperature = [bit_string[i:i+7] for bit_string in bits for i in range(0, len(bi
 moisture_percentages = [str(round((int(bit_string, 2) / 127) * 100, 0)) + '%' for bit_string in moisture]
 temperature_values = [int(bit_string[:6], 2) - 32 - (offset) + (0.5 if bit_string[-1] == '1' else 0) for bit_string in temperature]
 
-print(temperature_values)
-print(moisture_percentages)
-print(times)
+
